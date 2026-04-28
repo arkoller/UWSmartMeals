949f60fdab0081cde719d6bb862be74c4df39f8f
hauctest <mshauck@wisc.edu>
2026-04-27 21:00:09 -0500

    Added app.js and firebaseConfig

---
+++ app.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCX9y6lSSzClAXx4HZcXcx4Um5ryBv6eZk",
+  authDomain: "is424-uwsmartmeals.firebaseapp.com",
+  projectId: "is424-uwsmartmeals",
+  storageBucket: "is424-uwsmartmeals.firebasestorage.app",
+  messagingSenderId: "937787418296",
+  appId: "1:937787418296:web:d6b30348532c18dce4364d",
+};
