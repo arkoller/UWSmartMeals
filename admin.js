31a8db855cf0e6f0aac13c07ce8342452410af0d
helenaw <helena@test.com>
2026-04-28 18:30:38 -0500

    admin change

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 // Admin page logic for adding and deleting recipes from Firebase meal_ideas
 
-console.log("admin.js is connected");
+// console.log("admin.js is connected");
 
 const adminMessage = document.querySelector("#adminMessage");
 const recipeForm = document.querySelector("#addRecipeForm");
