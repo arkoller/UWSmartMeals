cb9838d596cb8c457e4c58a1ae6b16a9bf4afdb4
Amber <arkoller@wisc.edu>
2026-04-27 21:36:31 -0500

    tetstingcollections

--- app.js
+++ app.js
@@ -1,8 +1,71 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCX9y6lSSzClAXx4HZcXcx4Um5ryBv6eZk",
-  authDomain: "is424-uwsmartmeals.firebaseapp.com",
-  projectId: "is424-uwsmartmeals",
-  storageBucket: "is424-uwsmartmeals.firebasestorage.app",
-  messagingSenderId: "937787418296",
-  appId: "1:937787418296:web:d6b30348532c18dce4364d",
+// User Accounts Database
+
+let david_id = {
+  name: "David Smith",
+  email: "dsmith@gmail.com",
+  password: "password123",
 };
+
+let jane_id = {
+  name: "Jane Doe",
+  email: "jjdoe@gmail.com",
+  password: "pass1",
+};
+
+db.collection("user_accounts").doc("david_id").set(david_id);
+db.collection("user_accounts").doc("jane_id").set(jane_id);
+
+// Meal Ideas Database
+
+let avocado_egg = {
+  meal_name: "Avocado Egg Toast",
+  description:
+    "Toast with mashed avocado, hard-boiled eggs, strawberries, and milk.",
+  grocery_item: ["Bread", "Avocados", "Eggs", "Strawberries", "Milk"],
+  image_id: "link.com",
+};
+
+let french_toast = {
+  meal_name: "French Toast & Sausage",
+  description: "Cinnamon French toast with sausage, bananas, and milk.",
+  grocery_item: [
+    "Bread",
+    "Eggs",
+    "Milk",
+    "Cinnamon",
+    "Sugar",
+    "Butter",
+    "Maple Syrup",
+    "Sausage",
+    "Bananas",
+  ],
+  image_id: "link.com",
+};
+
+db.collection("meal_ideas").doc("avocado_egg").set(avocado_egg);
+db.collection("meal_ideas").doc("french_toast").set(french_toast);
+
+// Grocery List
+
+let david_list = {
+  user_id: "david_id",
+  grocery_item: ["Bread", "Avocados", "Eggs", "Strawberries", "Milk"],
+};
+
+let jane_list = {
+  user_id: "jane_id",
+  grocery_list: [
+    "Bread",
+    "Eggs",
+    "Milk",
+    "Cinnamon",
+    "Sugar",
+    "Butter",
+    "Maple Syrup",
+    "Sausage",
+    "Bananas",
+  ],
+};
+
+db.collection("grocery_list").doc("david_list").set(david_list);
+db.collection("grocery_list").doc("jane_list").set(jane_list);
